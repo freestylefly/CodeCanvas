402cf91cccc639cacbe01d4af30ecb7fd6f8d7ef
freestylefly <2689458656@qq.com>
2018-11-26 13:13:22 +0800

    Create UsersService.java

---
+++ MySQL数据库/JDBC操作数据库/03.DBCP连接池/登录注册按例/src/service/UsersService.java
@@ -0,0 +1,6 @@
+package service;
+//Âß¼­²ã½Ó¿Ú
+public interface UsersService {
+	public boolean login(String username,String password);
+	public boolean register(String username,String password);
+}

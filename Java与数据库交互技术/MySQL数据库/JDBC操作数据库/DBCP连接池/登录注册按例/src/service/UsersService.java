2ddcc0ef62dc49d78b3da07e8a92df2d3f37aa29
canghe_glb <2689458656_glb@qq.com>
2019-04-01 22:02:01 +0800

    去除非必要资料

--- Java与数据库交互技术/MySQL数据库/JDBC操作数据库/DBCP连接池/登录注册按例/src/service/UsersService.java
+++
@@ -1,6 +0,0 @@
-package service;
-//Âß¼­²ã½Ó¿Ú
-public interface UsersService {
-	public boolean login(String username,String password);
-	public boolean register(String username,String password);
-}
